763e51c2ec7b8e74e0adbf9481b662408fdccf18
Kaushal Devangan <kaushal_devangan@softprodigy.com>
2022-09-07 17:33:07 +0530

    Fixed image base url upload issue.

--- app/src/main/java/com/softprodigy/ballerapp/ui/features/home/teams/leaderboard/LeaderBoardViewModel.kt
+++ app/src/main/java/com/softprodigy/ballerapp/ui/features/home/teams/leaderboard/LeaderBoardViewModel.kt
@@ -26,49 +26,49 @@ class LeaderBoardViewModel @Inject constructor() : ViewModel() {
                 LeaderBoard(
                     "1111111111",
                     "Joe",
-                    "http://ballerapp.s3.us-east-2.amazonaws.com/teamLogo/1662473370996-IMG_20220805_120020_710.jpg",
+                    "profileImage/1662458857474-selected_image_4465069980681186921.jpg",
                     "1490"
                 ),
                 LeaderBoard(
                     "222222222",
                     "Charlie",
-                    "http://ballerapp.s3.us-east-2.amazonaws.com/teamLogo/1662473370996-IMG_20220805_120020_710.jpg",
+                    "profileImage/1662458857474-selected_image_4465069980681186921.jpg",
                     "3249"
                 ),
                 LeaderBoard(
                     "33333333",
                     "George",
-                    "http://ballerapp.s3.us-east-2.amazonaws.com/teamLogo/1662473370996-IMG_20220805_120020_710.jpg",
+                    "profileImage/1662458857474-selected_image_4465069980681186921.jpg",
                     "7936"
                 ),
                 LeaderBoard(
                     "444444444",
                     "Jacob",
-                    "http://ballerapp.s3.us-east-2.amazonaws.com/teamLogo/1662473370996-IMG_20220805_120020_710.jpg",
+                    "profileImage/1662458857474-selected_image_4465069980681186921.jpg",
                     "2343"
                 ),
                 LeaderBoard(
                     "111111111",
                     "Joe",
-                    "http://ballerapp.s3.us-east-2.amazonaws.com/teamLogo/1662473370996-IMG_20220805_120020_710.jpg",
+                    "profileImage/1662458857474-selected_image_4465069980681186921.jpg",
                     "1490"
                 ),
                 LeaderBoard(
                     "22222222",
                     "Charlie",
-                    "http://ballerapp.s3.us-east-2.amazonaws.com/teamLogo/1662473370996-IMG_20220805_120020_710.jpg",
+                    "profileImage/1662458857474-selected_image_4465069980681186921.jpg",
                     "3249"
                 ),
                 LeaderBoard(
                     "3333333",
                     "George",
-                    "http://ballerapp.s3.us-east-2.amazonaws.com/teamLogo/1662473370996-IMG_20220805_120020_710.jpg",
+                    "profileImage/1662458857474-selected_image_4465069980681186921.jpg",
                     "7936"
                 ),
                 LeaderBoard(
                     "44444444",
                     "Jacob",
-                    "http://ballerapp.s3.us-east-2.amazonaws.com/teamLogo/1662473370996-IMG_20220805_120020_710.jpg",
+                    "profileImage/1662458857474-selected_image_4465069980681186921.jpg",
                     "2343"
                 )
             )
