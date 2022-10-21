5657fff1177b3215f933af359162ab940f4eb538
Amarinder <amarinder_singh1@softprodigy.com>
2022-10-21 13:43:11 +0530

    Working on CR changes for invite player screen

--- app/src/main/java/com/softprodigy/ballerapp/ui/features/home/events/opportunities/EventRefereeRegistrationScreen.kt
+++ app/src/main/java/com/softprodigy/ballerapp/ui/features/home/events/opportunities/EventRefereeRegistrationScreen.kt
@@ -20,7 +20,6 @@ import com.softprodigy.ballerapp.ui.features.components.*
 import com.softprodigy.ballerapp.ui.features.home.events.EvEvents
 import com.softprodigy.ballerapp.ui.features.home.events.EventChannel
 import com.softprodigy.ballerapp.ui.features.home.events.EventViewModel
-import com.softprodigy.ballerapp.ui.features.home.invitation.InvitationEvent
 import com.softprodigy.ballerapp.ui.theme.ColorMainPrimary
 import com.softprodigy.ballerapp.ui.theme.md_theme_light_onSurface
 
@@ -185,7 +184,8 @@ fun EventRefereeRegistrationScreen(vm: EventViewModel, onNextScreen: () -> Unit)
             title = stringResource(id = R.string.select_role),
             selected = state.selectedRole,
             showLoading = state.showLoading,
-            roleList = state.roles
+            roleList = state.roles,
+            "",""
         )
     }
 }
